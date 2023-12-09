38ff995be95370dc0c49606e6ec2df30b5cedeff
radall <radall@outlook.com>
2023-12-09 22:39:06 +0000

    add connection string

---
+++ Common/Config/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Config;
+
+public class Configuration
+{
+    public required DatabaseConfig Database { get; set; }
+
+    public class DatabaseConfig
+    {
+        public required string ConnectionString { get; set; }
+    }
+}
+
